22a2b1845dafdc0bab45f42e6fa51caf9b9f6c5f
Max Leonard Inden <inden@innoscale.de>
2016-04-29 10:28:53 +0200

    Refactor elements.js

--- imports/api/elements.js
+++ imports/api/elements.js
@@ -5,26 +5,26 @@ const Elements = {};
 Elements.collection = new Meteor.Collection('Elements');
 
 Elements.types = [
-  {humanName: 'Reference object', name: 'referenceObject'},
-  {humanName: 'Hierarchy', name: 'hierarchy'},
+  { humanName: 'Reference object', name: 'referenceObject' },
+  { humanName: 'Hierarchy', name: 'hierarchy' },
 ];
 
+Elements.add = function add(parentId, typeName) {
+  const elementId = Elements.collection.insert(
+    { parentId: parentId, typeName: typeName });
+  Elements.collection.update(
+    { _id: parentId }, { $addToSet: { childrenIds: elementId } }
+  );
+  return elementId;
+};
+
 Elements.types.nameToHumanName = (name) => {
-  let type = Elements.types.find(function(type) {
-    if(type.name === name) {
+  const type = Elements.types.find(function(type) {
+    if (type.name === name) {
       return true;
     }
   });
   return type.humanName;
-};
-
-
-
-Elements.add = function add(parentId, typeName) {
-  let elementId = Elements.collection.insert(
-    {parentId: parentId, typeName: typeName});
-  Elements.collection.update(
-    {_id: parentId}, {$addToSet: {childrenIds: elementId}});
 };
 
 export default Elements;
