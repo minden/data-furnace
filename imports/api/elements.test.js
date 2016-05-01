a19e0cf2d054f3319fbf3317cc28ddff30dc0414
Max Leonard Inden <IndenML@gmail.com>
2016-05-01 09:25:26 +0200

    Add test for Elements.remove

--- imports/api/elements.test.js
+++ imports/api/elements.test.js
@@ -2,7 +2,7 @@
 
 import { Meteor } from 'meteor/meteor';
 import { describe, it } from 'meteor/practicalmeteor:mocha';
-import { should } from 'meteor/practicalmeteor:chai';
+import { should, expect } from 'meteor/practicalmeteor:chai';
 should();
 import Elements from './elements.js';
 
@@ -19,12 +19,25 @@ if (Meteor.isServer) {
     });
   });
 
-  describe.skip('Elements.add', () => {
+  describe('Elements.add', () => {
     it('should insert an element into the MongoDB', () => {
       const element = { parentId: null, typeName: 'hierarchy' };
       element._id = Elements.add(element.parentId, element.typeName);
       const elementFromDB = Elements.collection.findOne(element._id);
       elementFromDB.typeName.should.equal(element.typeName);
+    });
+  });
+
+  describe('Elements.remove', () => {
+    let elementId;
+
+    before(() => {
+      elementId = Elements.add(undefined, 'hierarchy');
+      Elements.remove(elementId, undefined);
+    });
+
+    it('should remove an Element from the database', () => {
+      expect(Elements.collection.findOne(elementId)).to.not.exist;
     });
   });
 
