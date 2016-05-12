07104c18baafc6cbea28351d63a6dbf5a8721e20
Max Leonard Inden <IndenML@gmail.com>
2016-05-12 19:14:53 +0200

    Add description field to element details

--- imports/ui/structure-view/element-details/element-details.jsx
+++ imports/ui/structure-view/element-details/element-details.jsx
@@ -5,22 +5,36 @@ import { Form, FormGroup, Col, Panel } from 'react-bootstrap';
 import Elements from '../../../api/elements.js';
 import ElementDetailsTitle from './element-details-title.jsx';
 import ElementTypeNameLabel from '../element-type-name-label.jsx';
+import InplaceEdit from '../../components/inplace-edit.jsx';
 
 const elementDetailsTitle = (props) => {
   return new ElementDetailsTitle({ elementName: props.element.name, elementId: props.elementId });
 };
+
 
 const ElementDetails = (props) => (
   <Panel
     header={elementDetailsTitle(props)}
   >
     <Form horizontal>
-      <FormGroup controlId="formHorizontalEmail">
+      <FormGroup>
         <Col sm={2} style={{ textAlign: 'right' }}>
           Type
         </Col>
         <Col sm={10}>
           <ElementTypeNameLabel typeName={props.element.typeName} />
+        </Col>
+      </FormGroup>
+
+      <FormGroup>
+        <Col sm={2} style={{ textAlign: 'right' }}>
+          Description
+        </Col>
+        <Col sm={10}>
+          <InplaceEdit
+            text={props.element.description}
+            onChange={(text) => Elements.setDescription(props.element._id, text)}
+          />
         </Col>
       </FormGroup>
     </Form>
