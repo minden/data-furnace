8997adfa4ebbc09f8b03f8b16cc5b1ab8825a806
Max Leonard Inden <IndenML@gmail.com>
2016-05-22 10:18:45 +0200

    Add measure collection to measure explorer

--- imports/ui/measure-view/measure-explorer.jsx
+++ imports/ui/measure-view/measure-explorer.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
+import React, { PropTypes } from 'react';
+import Measures from '../../api/measures/measures.js';
+import { Button, Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { createContainer } from 'meteor/react-meteor-data';
+
+const panelHeader = (props) => (
+  <div>
+    Measures
+    <Button
+      className="glyphicon glyphicon-plus pull-right"
+      style={{ padding: '0px', border: '0px', backgroundColor: 'transparent' }}
+      onClick={() => Measures.add()}
+    />
+  </div>
+);
 
 const MeasureExplorer = (props) => (
-  <Panel header="Measures">
+  <Panel header={panelHeader()}>
     <ListGroup>
-      <ListGroupItem>My first item</ListGroupItem>
+      {props.measures.map((measure) => {
+        return (
+          <ListGroupItem>{measure.name}</ListGroupItem>
+          );
+      })}
     </ListGroup>
   </Panel>
 );
 
-export default MeasureExplorer;
+MeasureExplorer.propTypes = {
+  measures: PropTypes.array.isRequired,
+};
 
+export default createContainer(() => {
+  return {
+    measures: Measures.collection.find({}).fetch(),
+  };
+}, MeasureExplorer);
