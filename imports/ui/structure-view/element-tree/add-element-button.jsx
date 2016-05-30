6feb37adea29437037552d02f10c56f5320235c7
Max Leonard Inden <inden@innoscale.de>
2016-05-30 18:02:32 +0200

    Make add-element-button a stateless function

--- imports/ui/structure-view/element-tree/add-element-button.jsx
+++ imports/ui/structure-view/element-tree/add-element-button.jsx
@@ -2,49 +2,45 @@ import React from 'react';
 import { DropdownButton, MenuItem } from 'react-bootstrap';
 import Elements from '../../../api/elements/elements.js';
 
-export default class AddElementButton extends React.Component {
-  constructor(props) {
-    super(props);
+const AddElementButton = (props) => {
+  if (props.possibleTypeNames.length === 0) {
+    return null;
   }
 
-  render() {
-    const possibleTypes = Elements.types.filter((type) =>
-      this.props.possibleTypeNames.indexOf(type.name) !== -1
-    );
+  const possibleTypes = Elements.types.filter(
+    (type) => props.possibleTypeNames.indexOf(type.name) !== -1
+  );
 
-    const dropDownButtonStyle = {
-      padding: '0px',
-      border: '0px',
-      backgroundColor: 'transparent',
-    };
+  const dropDownButtonStyle = {
+    padding: '0px',
+    border: '0px',
+    backgroundColor: 'transparent',
+  };
 
-    if (this.props.possibleTypeNames.length === 0) {
-      return null;
-    }
-
-    return (
-      <DropdownButton
-        className="glyphicon glyphicon-plus add-element-button"
-        id="add-element-dropdown"
-        onSelect={(typeName) => Elements.add(this.props.elementId, typeName)}
-        title=""
-        style={dropDownButtonStyle}
-      >
-        {possibleTypes.map((elementType) => {
-          return (
-            <MenuItem
-              eventKey={elementType.name}
-              key={elementType.name}
-            >
-              {elementType.humanName}
-            </MenuItem>);
-        })}
-      </DropdownButton>
-    );
-  }
-}
+  return (
+    <DropdownButton
+      className="glyphicon glyphicon-plus add-element-button"
+      id="add-element-dropdown"
+      onSelect={(typeName) => Elements.add(props.elementId, typeName)}
+      title=""
+      style={dropDownButtonStyle}
+    >
+      {possibleTypes.map((elementType) => {
+        return (
+          <MenuItem
+            eventKey={elementType.name}
+            key={elementType.name}
+          >
+            {elementType.humanName}
+          </MenuItem>);
+      })}
+    </DropdownButton>
+  );
+};
 
 AddElementButton.propTypes = {
   elementId: React.PropTypes.string,
   possibleTypeNames: React.PropTypes.array.isRequired,
 };
+
+export default AddElementButton;
