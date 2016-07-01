778dd12a22e2a63451def2878bcc87cf6019ab35
Max Leonard Inden <IndenML@gmail.com>
2016-07-01 22:53:39 +0200

    Add test for ElementRows component

---
+++ imports/ui/report-view/table/element-rows.test.jsx
@@ -0,0 +1,45 @@
+/* eslint-env mocha */
+import { shallow } from 'enzyme';
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import Reports from '../../../api/reports/reports.js';
+import Elements from '../../../api/elements/elements.js';
+import elementRows from './element-rows.jsx';
+import { Table } from 'react-bootstrap';
+
+describe('ElementRows', () => {
+  if (Meteor.isServer) return;
+  let rows;
+
+  before(() => {
+    const reportId = Reports.add();
+
+    const elementOneId = Elements.add(undefined, 'dimension');
+    Elements.characteristics.add(elementOneId, '2015');
+    Elements.characteristics.add(elementOneId, '2016');
+    Reports.addToTable(reportId, 'element', elementOneId);
+
+    const elementTwoId = Elements.add(undefined, 'dimension');
+    Elements.characteristics.add(elementTwoId, 'Jan');
+    Elements.characteristics.add(elementTwoId, 'Feb');
+    Reports.addToTable(reportId, 'element', elementTwoId);
+
+    const report = Reports.collection.findOne(reportId);
+    rows = shallow(<Table>{elementRows(reportId, report.elements)}</Table>);
+  });
+
+  describe('with two elements two characteristics each', () => {
+    it('returns two rows', () => {
+      rows.find('tr').should.have.length(2);
+    });
+
+    it('returns five cells in the first row', () => {
+      rows.find('tr').at(0).find('td').should.have.length(5);
+    });
+
+    it('returns seven cells in the second row', () => {
+      // Hotfix to count cells. Duplicated cells are ignored otherwise.
+      rows.find('tr').at(1).html().match(/<td/g).should.have.length(7);
+    });
+  });
+});
