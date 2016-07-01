e326f59a23ccdd58782d58096dc2b7bf5e3523f3
Max Leonard Inden <IndenML@gmail.com>
2016-07-01 21:56:44 +0200

    Add test for report table

---
+++ imports/ui/report-view/table/table.test.jsx
@@ -0,0 +1,38 @@
+/* eslint-env mocha */
+import { mount } from 'enzyme';
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import Reports from '../../../api/reports/reports.js';
+import Elements from '../../../api/elements/elements.js';
+import Measures from '../../../api/measures/measures.js';
+import ReportTable from './table.jsx';
+import { Table } from 'react-bootstrap';
+
+describe('ReportTable', () => {
+  if (Meteor.isServer) return;
+  let table;
+  let measureId;
+
+  before(() => {
+    const reportId = Reports.add();
+    const elementId = Elements.add(undefined, 'dimension');
+    measureId = Measures.add();
+    Measures.setName(measureId, 'The measure name');
+    Reports.addToTable(reportId, 'element', elementId);
+    Reports.addToTable(reportId, 'measure', measureId);
+    const report = Reports.collection.findOne(reportId);
+    table = mount(<ReportTable report={report} />);
+  });
+
+  it('contains a table', () => {
+    table.find(Table).should.have.length(1);
+  });
+
+  it('has two rows', () => {
+    table.find('tr').should.have.length(2);
+  });
+
+  it('displays the name of the measure', () => {
+    table.find('tr').at(1).text().should.equal('The measure name');
+  });
+});
