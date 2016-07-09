d30c2658d1c2be98582d54476918a9f0a18710e6
Max Leonard Inden <IndenML@gmail.com>
2016-07-09 15:38:14 +0200

    Style cursor placeholder

--- imports/ui/measure-view/measure-editor/expressions/cursor-placeholder.jsx
+++ imports/ui/measure-view/measure-editor/expressions/cursor-placeholder.jsx
@@ -1,15 +1,68 @@
 import React, { PropTypes } from 'react';
 import { Button } from 'react-bootstrap';
 
-const CursorPlaceholder = (props) => (
-  <Button onClick={() => props.setCursor({ expressionIdBefore: props.expressionId })} >
-    {isActive(props.cursor, props.expressionId)}
-  </Button>
-);
+class CursorPlaceholder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: false,
+    };
+  }
 
-const isActive = (cursor, expressionId) => {
-  if (cursor.expressionIdBefore === expressionId) return '|';
-};
+  componentDidMount() {
+    this.shouldCursorFlash();
+  }
+
+  componentDidUpdate() {
+    this.shouldCursorFlash();
+  }
+
+  componentWillUnMount() {
+    this.shouldCursorFlash();
+  }
+
+  shouldCursorFlash() {
+    if (this.isActive()) {
+      if (this.cursorInterval) return;
+      this.cursorInterval = setInterval(() => {
+        this.setState({ on: !this.state.on });
+      }, 800);
+    } else {
+      if (this.cursorInterval) {
+        clearInterval(this.cursorInterval);
+        this.cursorInterval = undefined;
+        this.setState({ on: false });
+      }
+    }
+  }
+
+  isActive() {
+    return this.props.cursor.expressionIdBefore === this.props.expressionId;
+  }
+
+  cursorStyle() {
+    return {
+      color: (this.state.on ? 'black' : 'white'),
+      border: 'none',
+      backgroundColor: 'white',
+      cursor: 'text',
+      outline: 'none',
+    };
+  }
+
+  render() {
+    return (
+      <Button
+        className="fa"
+        style={this.cursorStyle()}
+        onClick={() => this.props.setCursor({ expressionIdBefore: this.props.expressionId })}
+      >
+        |
+      </Button>
+    );
+  }
+}
+
 
 CursorPlaceholder.propTypes = {
   cursor: PropTypes.object.isRequired,
