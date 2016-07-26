0c17092fe3865c40dea3588b3c855d39b6f25c56
Max Leonard Inden <IndenML@gmail.com>
2016-07-26 12:42:51 +0200

    Update test environment to enzyme

--- imports/ui/measure-view/measure-explorer.test.jsx
+++ imports/ui/measure-view/measure-explorer.test.jsx
@@ -3,78 +3,70 @@
 import $ from 'jquery';
 import MeasureExplorer from './measure-explorer.jsx';
 import Measures from '../../api/measures/measures.js';
+import { mount } from 'enzyme';
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
 import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 import { should } from 'chai';
+import { ListGroupItem } from 'react-bootstrap';
+import subscribeAndWait from '../../helpers/subscribe-and-wait.js';
 should();
 
-if (Meteor.isClient) {
-  describe('Measure explorer', () => {
-    let measureExplorer;
-    let setSelectedMeasureIdFunctionCalled = false;
-    before(() => {
-      const testEnvironment = document.createElement('div');
-      testEnvironment.setAttribute('id', 'test-environment');
-      document.body.appendChild(testEnvironment);
-      render(
-        <MeasureExplorer
-          setSelectedMeasureId={function setSelected() {
-            setSelectedMeasureIdFunctionCalled = true;
-          }}
-        />,
-        document.getElementById('test-environment')
-      );
-      measureExplorer = $('#test-environment .panel');
-    });
+describe('Measure explorer', () => {
+  if (Meteor.isServer) return;
 
-    before((done) => {
-      resetDatabase(null, () => done());
-    });
+  let measureExplorer;
+  let setSelectedMeasureIdFunctionCalled = false;
 
-    after(() => {
-      $('#test-environment').remove();
-    });
-
-    it('should start of with a clean measure collection', () => {
-      Measures.collection.find().count().should.equal(0);
-    });
-
-    it('should exist', () => {
-      measureExplorer.length.should.be.above(0);
-    });
-
-    describe('the plus button', () => {
-      before(() => {
-        TestUtils.Simulate.click($('button.glyphicon-plus')[0]);
-      });
-
-      it('should insert a new measure into the db', () => {
-        Measures.collection.find().count().should.equal(1);
-      });
-
-      it('should create a new measure in the ui', (done) => {
-        const interval = setInterval(() => {
-          if ($('.list-group div').length === 1) {
-            clearInterval(interval);
-            true.should.equal(true);
-            done();
-          }
-        }, 0);
-      });
-    });
-
-    describe('the measure list', () => {
-      before(() => {
-        Measures.add();
-        $('.list-group div')[0].click();
-      });
-
-      it('calls the setSelectedElementId() function, when measure is clicked', () => {
-        setSelectedMeasureIdFunctionCalled.should.equal(true);
-      });
+  before((done) => {
+    measureExplorer = mount(
+      <MeasureExplorer
+        setSelectedMeasureId={function setSelected() {
+          setSelectedMeasureIdFunctionCalled = true;
+        }}
+      />,
+    );
+    resetDatabase(null, () => {
+      subscribeAndWait(['measures'], done);
     });
   });
-}
+
+  after(() => {
+    $('#test-environment').remove();
+  });
+
+  it('should start of with a clean measure collection', () => {
+    Measures.collection.find().count().should.equal(0);
+  });
+
+  describe('the plus button', () => {
+    before(() => {
+      measureExplorer.find('button.glyphicon-plus').simulate('click');
+    });
+
+    it('should insert a new measure into the db', () => {
+      Measures.collection.find().count().should.equal(1);
+    });
+
+    it('should create a new measure in the ui', (done) => {
+      const interval = setInterval(() => {
+        if (measureExplorer.find(ListGroupItem).length === 1) {
+          clearInterval(interval);
+          true.should.equal(true);
+          done();
+        }
+      }, 100);
+    });
+  });
+
+  describe('the measure list', () => {
+    before(() => {
+      Measures.add();
+      measureExplorer.find('.list-group div').first().simulate('click');
+    });
+
+    it('calls the setSelectedElementId() function, when measure is clicked', () => {
+      setSelectedMeasureIdFunctionCalled.should.equal(true);
+    });
+  });
+});
