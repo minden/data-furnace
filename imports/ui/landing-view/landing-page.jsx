4d406199cfe7e7cbb6ab237eb3fc2f7f710343ab
Max Leonard Inden <IndenML@gmail.com>
2016-08-06 14:18:10 +0200

    Remove unneeded imports

--- imports/ui/landing-view/landing-page.jsx
+++ imports/ui/landing-view/landing-page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron, PageHeader, Grid } from 'react-bootstrap';
+import { Jumbotron, Grid } from 'react-bootstrap';
 
 const LandingPage = () => {
   return (
